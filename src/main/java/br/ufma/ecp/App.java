5cfa4a2043c921db43df7761dfae71fe8059086e
Paulo301 <paulovictor1918@gmail.com>
2022-07-06 20:02:15 -0300

    Arquivos da pasta 11 compilados

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -1,14 +1,34 @@
 package br.ufma.ecp;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public class App 
 {
+    public static void saveToFile(String fileName, String output) {
+  
+       
+        FileOutputStream outputStream;
+        try {
+            outputStream = new FileOutputStream(fileName);
+            byte[] strToBytes = output.getBytes();
+            outputStream.write(strToBytes);
+    
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-    private static String fromFile() {
-        File file = new File("Main.jack");
+    private static String fromFile(File file) {        
 
         byte[] bytes;
         try {
@@ -21,8 +41,56 @@ public class App
         return "";
     } 
 
-    public static void main( String[] args )
-    {
-        
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Please provide a single file path argument.");
+            System.exit(1);
+        }
+
+        File file = new File(args[0]);
+        // File file = new File("/home/paulo/jackcompiler-java/src/test/resources/11/Square");
+
+        if (!file.exists()) {
+            System.err.println("The file doesn't exist.");
+            System.exit(1);
+        }
+
+        // we need to compile every file in the directory
+        if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                if (f.isFile() && f.getName().endsWith(".jack")) {
+
+                    var inputFileName = f.getAbsolutePath();
+                    var pos = inputFileName.indexOf('.');
+                    var outputFileName = inputFileName.substring(0, pos) + ".vm";
+                    
+                    System.out.println("compiling " +  inputFileName);
+                    var input = fromFile(f);
+                    var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+                    parser.parse();
+                    var result = parser.VMOutput();
+                    saveToFile(outputFileName, result);
+                }
+
+            }
+        // we only compile the single file
+        } else if (file.isFile()) {
+            if (!file.getName().endsWith(".jack"))  {
+                System.err.println("Please provide a file name ending with .jack");
+                System.exit(1);
+            } else {
+                var inputFileName = file.getAbsolutePath();
+                var pos = inputFileName.indexOf('.');
+                var outputFileName = inputFileName.substring(0, pos) + ".vm";
+                
+                System.out.println("compiling " +  inputFileName);
+                var input = fromFile(file);
+                var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+                parser.parse();
+                var result = parser.VMOutput();
+                saveToFile(outputFileName, result);
+                
+            }
+        }
     }
 }
